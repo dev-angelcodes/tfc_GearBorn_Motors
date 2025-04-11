e73055eee46bb017e647fc8946a467edc8f7a7eb
Angel Fernandez <angelfernandezblanco16@gmail.com>
2025-04-11 15:46:37 +0200

    Implemeto la carpeta para el codigo del backend

---
+++ Backend/GearbornMotorsAPI/src/main/java/com/gearborn/motors/gearbornMotors/GearbornMotorsApplication.java
@@ -0,0 +1,13 @@
+package com.gearborn.motors.gearbornMotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearbornMotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearbornMotorsApplication.class, args);
+	}
+
+}
