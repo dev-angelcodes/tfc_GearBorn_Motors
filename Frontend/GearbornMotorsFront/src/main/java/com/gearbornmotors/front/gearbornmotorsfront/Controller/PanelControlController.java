24f153909543c0740bf1383f631575aca3757f49
Angel Fernandez <angelfernandezblanco16@gmail.com>
2025-04-25 17:00:05 +0200

    Creo la escena de RegistroEmpleados y la llamo desde PanelControl

--- Frontend/GearbornMotorsFront/src/main/java/com/gearbornmotors/front/gearbornmotorsfront/Controller/PanelControlController.java
+++ Frontend/GearbornMotorsFront/src/main/java/com/gearbornmotors/front/gearbornmotorsfront/Controller/PanelControlController.java
@@ -1,4 +1,27 @@
 package com.gearbornmotors.front.gearbornmotorsfront.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class PanelControlController {
+
+    @FXML  public StackPane contenidoControl;
+
+
+    @FXML
+    public void mostrarRegistro(ActionEvent event) {
+        try {
+            Parent registroEmpleado = FXMLLoader.load(Objects.requireNonNull(getClass()
+                    .getResource("/com/gearbornmotors/front/gearbornmotorsfront/fxml/RegistroEmpleado.fxml")));
+            contenidoControl.getChildren().setAll(registroEmpleado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
